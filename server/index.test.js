c16af5542188625534c63139ddb19372051dbab4
Huy Truong <hgtruong@ucdavis.edu>
2021-01-30 15:32:07 -0800

    setting up API test calls

--- server/index.test.js
+++ server/index.test.js
@@ -1,1 +1,17 @@
-const axios = require('axios');
+const app = require('./index');
+
+describe('API test calls', () => {
+  it('should return array of makes greater than 2', async (done) => {
+
+  });
+  
+  // it('should return array of models greater than 2 for make: Acura', async () => {
+  //   await request(app)
+  //   .get("/models?selectedMake=acura")
+  //   .expect(200)
+  //   .then((response) => {
+  //     expect(response.body.lenght).toBeGreaterThan(1);
+  //   })
+  //   done();
+  // });
+});
